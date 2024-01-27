47da46037378c3835a821dd67229ba8f9551f8a7
Αλέξανδρος <115009236+AlexandrosMo@users.noreply.github.com>
2024-01-27 22:55:30 +0200

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,22 @@
+price = float(input("Enter the price: "))
+
+class Discount:
+    def __init__(self, price):
+        self.price = price
+    
+    def calculate_discount(self):
+        discount_amount = 0.50 if self.price >= 2.00 else 0.25  # Εκπτώσεις ανάλογα με την τιμή
+        return discount_amount
+    
+    def calculate_final_price(self):
+        discount_amount = self.calculate_discount()
+        final_price = self.price - discount_amount
+        return final_price
+    
+def main():
+    shop_discount = Discount(price)
+    final_price = shop_discount.calculate_final_price()
+    print(f"Final price: {final_price:.2f}€")
+
+if __name__ == "__main__":
+    main()
